0a51cacba75c84680c25b309ef2aab4b51197b81
Garidepalli Laxmi Sampath <laxmisampath806@gmail.com>
2024-12-12 22:59:58 +0530

    Add files via upload

---
+++ Button Maker.js
@@ -0,0 +1,25 @@
+let backGroundColorInput = document.getElementById("bgColorInput");
+let fontColorInput = document.getElementById("fontColorInput");
+let fontSizeInput = document.getElementById("fontSizeInput");
+let fontWeightInput = document.getElementById("fontWeightInput");
+let paddingInput = document.getElementById("paddingInput");
+let borderRadiusInput = document.getElementById("borderRadiusInput");
+let customButton = document.getElementById("customButton");
+
+function newButton() {
+
+    let bgColor = backGroundColorInput.value;
+    let fontColor = fontColorInput.value;
+    let fontSize = fontSizeInput.value;
+    let fontWeight = fontWeightInput.value;
+    let padding = paddingInput.value;
+    let borderRadius = borderRadiusInput.value;
+
+    customButton.style.backgroundColor = bgColor;
+    customButton.style.color = fontColor;
+    customButton.style.fontSize = fontSize;
+    customButton.style.fontWeight = fontWeight;
+    customButton.style.padding = padding;
+    customButton.style.borderRadius = borderRadius;
+
+}
